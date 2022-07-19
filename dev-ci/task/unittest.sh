bfb41be371c88bc833975c8f6f5f6fc0fdbf53dd
kodacme <kodac.saito@kodac.me>
2022-07-20 01:05:02 +0900

    fix: unittest.sh

--- dev-ci/task/unittest.sh
+++ dev-ci/task/unittest.sh
@@ -5,3 +5,8 @@ cd repo/webapp
 pip install --upgrade pip
 pip install -r requirements.txt
 pytest --junitxml=../../report-result/report.xml
+
+if [ $? -gt 0 ]; then
+    echo 'Test has failed'
+fi
+exit 0
